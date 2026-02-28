9cdf6e90278ffe5432a0089399681ff3d385b732
Cristina Zhang <zhangjinting118@gmail.com>
2026-02-28 23:54:06 +0100

    update

--- ContainerMostWater.py
+++ ContainerMostWater.py
@@ -8,6 +8,19 @@ class Solution:
                 area = min_con * (j-i)
                 res = max(res, area)
         return res
+    
+    def maxArea1(self, height):
+        max_area = 0
+        left = 0
+        right =len(height) - 1
+        while left < right:
+            max_area = max(max_area, (right - left) * min(height[left], height[right]))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_area
+
 
 height = [1,8,6,2,5,4,8,3,7]
 solution = Solution()
