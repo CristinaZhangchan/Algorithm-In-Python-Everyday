12156ff02a66396b2965f7ce574b70d820a0dac1
Cristina Zhang <zhangjinting118@gmail.com>
2026-03-11 15:49:59 +0100

    update

---
+++ 32.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestParenthese(self, s):
+        stack = [-1]
+        max_length = 0
+
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    max_length = max(max_length, i-stack[-1])
+
+        return max_length
+
+s = '()()()(())'
+solution = Solution()
+print(solution.longestParenthese(s))
