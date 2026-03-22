3fa12d87becfb279b9a724fb10e6a69d711ccc13
Cristina Zhang <zhangjinting118@gmail.com>
2026-03-22 12:34:35 +0100

    update

--- 41.py
+++ 41.py
@@ -1,16 +1,16 @@
 class Solution:
     def firstMissPos(self, nums):
-        nums = [n for n in nums if n>0]
+        nums = [n for n in nums if n > 0]
         nums.sort()
 
         target = 1
         for n in nums:
-            if target == n:
+            if n == target:
                 target += 1
-            elif n > target :
+            elif n > target:
                 return target
         return target
             
-nums = [1,2,3,5,7,8,9,11,12]
+nums = [0,2,3,5,7,8,9,11,12]
 solution = Solution()
 print(solution.firstMissPos(nums))
---
+++ 43.py
@@ -0,0 +1,11 @@
+class Solution:
+    def multiplyString(self, num1,num2):
+        num1 = int(num1)
+        num2 = int(num2)
+        return num1 * num2
+        
+            
+num1 = "123"
+num2 = "456"
+solution = Solution()
+print(solution.multiplyString(num1,num2))
